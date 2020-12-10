9fe1f89a9ed943253db19a5c686af69c62e9a7a6
elisabhty <elisablanchart@gmail.com>
2020-12-10 21:18:16 +0100

    project 2

---
+++ app/src/main/java/com/example/miage_mouhoun_yamina_blanchart_elisa/VoirRDV.java
@@ -0,0 +1,14 @@
+package com.example.miage_mouhoun_yamina_blanchart_elisa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VoirRDV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voir_r_d_v);
+    }
+}
